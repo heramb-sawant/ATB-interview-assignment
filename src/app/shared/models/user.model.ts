c8c22b50b749b4930bcaf03c14796f46cda01933
Heramb Sawant <hes278@mail.usask.ca>
2019-03-17 22:26:36 -0600

    refactor: replace email with username in user model

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,13 +1,13 @@
 export class User {
   id: string;
-  email: string;
+  username: string;
   firstName: string;
   lastName: string;
   balance: number;
 
-  constructor(id: string, email: string, firstName: string, lastName: string, balance: number) {
+  constructor(id: string, username: string, firstName: string, lastName: string, balance: number) {
     this.id = id;
-    this.email = email;
+    this.username = username;
     this.firstName = firstName;
     this.lastName = lastName;
     this.balance = balance;
