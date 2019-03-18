af06d2ed52bbf558d5b875868e9bb1907690a4b9
Heramb Sawant <hes278@mail.usask.ca>
2019-03-17 22:40:44 -0600

    feat: add authentication logic to login component

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../../shared/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +16,19 @@ export class LoginComponent {
   passwordForm = new FormControl('', [
     Validators.required
   ]);
+  displayCredentialsError: boolean = false;
 
-  constructor( ) { }
+  constructor(private router: Router, private authenticationService: AuthenticationService) { }
 
+  login() {
+    console.log('loggingin')
+    if (!this.usernameForm.hasError('required') && !this.passwordForm.hasError('required')) {
+      let isUserAuthenticated = this.authenticationService.login(this.usernameForm.value, this.passwordForm.value);
+      if (!isUserAuthenticated) {
+        this.displayCredentialsError = true;
+      } else {
+        this.router.navigateByUrl('/account');
+      }
+    }
+  }
 }
