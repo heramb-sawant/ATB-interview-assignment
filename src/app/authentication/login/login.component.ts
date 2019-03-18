7ec9f341454767eba9088eb205ff59d1006871ce
Heramb Sawant <hes278@mail.usask.ca>
2019-03-17 22:15:08 -0600

    feat: add login html template

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  usernameForm = new FormControl('', [
+    Validators.required
+  ]);
+  passwordForm = new FormControl('', [
+    Validators.required
+  ]);
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor( ) { }
 
 }
