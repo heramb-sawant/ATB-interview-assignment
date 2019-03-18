109eb2ec8cba9640ca0f8e88d70896c8030af7b7
Heramb Sawant <hes278@mail.usask.ca>
2019-03-17 22:17:42 -0600

    feat: add potential routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { SummaryComponent } from './account/summary/summary.component';
+import { TransactionsComponent } from './account/transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AuthenticationComponent },
+  { path: 'authentication', component: AuthenticationComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'account/summary', component: SummaryComponent },
+  { path: 'account/transactions', component: TransactionsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
