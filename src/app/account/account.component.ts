ad421141cce0ebec8b7dfd65695d3463acda6b59
Heramb Sawant <hes278@mail.usask.ca>
2019-03-18 22:16:13 -0600

    feat: add logout functionality

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AuthenticationService } from '../shared/services/authentication.service';
 import { User } from '../shared/models/user.model';
 import { UserService } from '../shared/services/user.service';
 
@@ -16,10 +17,16 @@ export class AccountComponent implements OnInit {
   constructor(
     private userService: UserService,
     private router: Router,
+    private authenticationService: AuthenticationService
   ) { }
 
   ngOnInit() {
     const userId: string = localStorage.getItem('authenticatedUserId');
     this.user = this.userService.getUser(userId);
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigateByUrl('/authentication');
   }
 }
